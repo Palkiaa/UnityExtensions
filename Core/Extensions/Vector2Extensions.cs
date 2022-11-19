eb4c70d0310381b522e8a9d0b3149a06ddb3d238
Palkia <4palkia4@gmail.com>
2022-11-19 17:42:49 +0200

    vector extensions for setting single axis values

--- Core/Extensions/Vector2Extensions.cs
+++ Core/Extensions/Vector2Extensions.cs
@@ -2,6 +2,20 @@
 {
     public static class Vector2Extensions
     {
+        #region Set
+
+        public static Vector2 SetX(this Vector2 vector, float x)
+        {
+            return new Vector3(x, vector.y);
+        }
+
+        public static Vector2 SetY(this Vector2 vector, float y)
+        {
+            return new Vector3(vector.x, y);
+        }
+
+        #endregion Set
+
         public static Vector2 Divide(this Vector2 a, Vector2 b)
         {
             return new Vector2(a.x / b.x, a.y / b.y);
