8a901969ea922b0aa43b9021d1883e42926c2616
Palkia <4palkia4@gmail.com>
2022-11-19 17:45:42 +0200

    added color extensions

---
+++ Core/Extensions/ColorExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace UnityEngine
+{
+    public static class ColorExtensions
+    {
+        #region Set
+
+        public static Color SetR(this Color color, float red)
+        {
+            return new Color(red, color.g, color.b, color.a);
+        }
+
+        public static Color SetG(this Color color, float green)
+        {
+            return new Color(color.r, green, color.b, color.a);
+        }
+
+        public static Color SetB(this Color color, float blue)
+        {
+            return new Color(color.r, color.g, blue, color.a);
+        }
+
+        public static Color SetA(this Color color, float alpha)
+        {
+            return new Color(color.r, color.g, color.b, alpha);
+        }
+
+        #endregion Set
+    }
+}
